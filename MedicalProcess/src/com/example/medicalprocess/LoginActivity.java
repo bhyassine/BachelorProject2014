d4c1dcd1c6d8583a37ff0e5476bf2d80f84d8c32
bhyassine <yassine.benhamadi@epfl.ch>
2014-05-21 20:36:06 +0200

    First commit of project

---
+++ MedicalProcess/src/com/example/medicalprocess/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.medicalprocess;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+	
+	/**
+	 * Called when the "Log In" button is pressed.
+	 * @param view
+	 */
+	public void logIn(View view) {
+	    /////// A faire par Yassine
+	}
+	
+	/**
+	 * Called when the "Sign Up" button is pressed.
+	 * Starts the SignUpActivity.
+	 * @param view
+	 */
+	public void signUp(View view) {
+	    Intent signUpActivityIntent = new Intent(this, SignUpActivity.class);
+	    startActivity(signUpActivityIntent);
+	}
+
+}
